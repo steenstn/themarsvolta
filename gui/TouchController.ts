531c6400c45e02053fd4db04bb9319bd1df06e13
steenstn <alest849@gmail.com>
2019-05-09 10:19:35 +0200

    Light up touchcontrollers when pressing

--- gui/TouchController.ts
+++ gui/TouchController.ts
@@ -1,9 +1,21 @@
 class TouchController implements GUIRenderable {
     
-    constructor(public readonly x: number, public readonly y: number, public readonly width : number, public readonly height : number, public readonly keyCode : KeyCode) {}
+    private active : boolean;
+    constructor(public readonly x: number, public readonly y: number, public readonly width : number, public readonly height : number, public readonly keyCode : KeyCode) {
+        this.active = false;
+    }
+
+    activate = () => {
+        this.active = true;
+    }
+
+    deactivate = () => {
+        this.active = false;
+    }
 
     render(context: any): void {
-        context.fillStyle= "rgba(255,255,255,0.4)";
+        let alpha = this.active ? 0.8 : 0.4;
+        context.fillStyle= "rgba(255,255,255,"+alpha+")";
         context.fillRect(this.x,this.y,this.width,this.height);
     }
 
--- js/gui/TouchController.js
+++ js/gui/TouchController.js
@@ -1,13 +1,22 @@
 var TouchController = (function () {
     function TouchController(x, y, width, height, keyCode) {
+        var _this = this;
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.keyCode = keyCode;
+        this.activate = function () {
+            _this.active = true;
+        };
+        this.deactivate = function () {
+            _this.active = false;
+        };
+        this.active = false;
     }
     TouchController.prototype.render = function (context) {
-        context.fillStyle = "rgba(255,255,255,0.4)";
+        var alpha = this.active ? 0.8 : 0.4;
+        context.fillStyle = "rgba(255,255,255," + alpha + ")";
         context.fillRect(this.x, this.y, this.width, this.height);
     };
     return TouchController;
